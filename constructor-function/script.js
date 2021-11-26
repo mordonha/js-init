27b459451c10e11691374a7141b619a41ddace7e
Paulo Marcos ordonha <marcos.ordonha@gmail.com>
2021-11-26 00:18:57 -0300

    Alterações parciais no exercicio

--- constructor-function/script.js
+++ constructor-function/script.js
@@ -1,14 +1,24 @@
 // Transforme o objeto abaixo em uma Constructor Function
-const pessoa = {
-  nome: 'Nome pessoa',
-  idade: 0,
-  andar() {
+function Pessoa (name, age){
+  this.nome = name;
+  this.idade = age;
+
+  this.andar = function() {
     console.log(this.nome + ' andou');
   }
 }
 
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
+
+var vector = [];
+var nameList = ['joao','maria','bruno'];
+var ageList = [20,25,15];
+
+for(let i = 0; i < nameList.length; i++){
+  vector[i] = new Pessoa(nameList[i], ageList[i])
+}
+
 
 
 // Crie uma Constructor Function (Dom) para manipulação
