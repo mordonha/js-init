246175bf154921e2b7b920f863f7035be0aa1b37
Paulo Marcos ordonha <marcos.ordonha@gmail.com>
2022-03-09 14:30:18 -0300

    test

--- Origamid - exerN/16 - Function Fun/script.js
+++ Origamid - exerN/16 - Function Fun/script.js
@@ -1,14 +1,13 @@
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
 
-
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
-
+........................Array...
 
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
-
+............Array.............
